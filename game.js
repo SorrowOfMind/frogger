a7914056016f9f02ba052b172f672aa8ec118f60
Basia <b.kumor@yahoo.com>
2020-09-23 17:45:43 +0200

    lanes

--- game.js
+++ game.js
@@ -7,6 +7,10 @@ const height = ctx.canvas.height;
 const SIZE = 20;
 
 const froggy = {
+    width: SIZE,
+    height: SIZE,
+    x: width*0.5 - this.width,
+    y: height - this.height,
 
 }
 
@@ -36,11 +40,18 @@ const drawLanes = () => {
     ctx.strokeStyle = "white";
     ctx.beginPath();
     
-    for (let i = 0; i < width; i++) {
-        ctx.moveTo(i*SIZE, SIZE*16);
-        for (let j = 0; j < 21; j++) {
-            ctx.lineTo(j, SIZE*16);
+    for (let i = 0; i < 10; i++) {
+        let x = i*SIZE;
+        let y = SIZE*16+i*22;
+        ctx.moveTo(0, y);
+        // ctx.lineTo(x+10, SIZE*16);
+        // ctx.stroke();
+        for (let j = 0; j < 30; j++) {
+            ctx.moveTo(j*SIZE, y);
+            ctx.lineTo(j*SIZE+10, y);
+            ctx.stroke();
         }
+
     }
     ctx.stroke();
 }
